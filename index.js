5eeb10f5ec78205047264537bfd070842dc619c8
Hirak Desai <53598504+hirak214@users.noreply.github.com>
2023-08-14 03:25:38 +0530

    Yashvi work

--- index.js
+++ index.js
@@ -24,22 +24,7 @@ const showAsideBtn = $('.show-side-btn')
 const sidebar = $('.sidebar')
 const wrapper = $('#wrapper')
 
-// showAsideBtn.addEventListener('click', function () {
-//   $(`#${this.dataset.show}`).classList.toggle('show-sidebar')
-//   wrapper.classList.toggle('fullwidth')
-// })
 
-// if (window.innerWidth < 767) {
-//   sidebar.classList.add('show-sidebar');
-// }
-
-// window.addEventListener('resize', function () {
-//   if (window.innerWidth > 767) {
-//     sidebar.classList.remove('show-sidebar')
-//   }
-// })
-
-// dropdown menu in the side nav
 var slideNavDropdown = $('.sidebar-dropdown');
 
 $('.sidebar .categories').addEventListener('click', function (event) {
@@ -74,11 +59,6 @@ $('.sidebar .categories').addEventListener('click', function (event) {
   } else {
     find(item, '.sidebar-dropdown').classList.toggle('active')
   }
-})
-
-$('.sidebar .close-aside').addEventListener('click', function () {
-  $(`#${this.dataset.close}`).classList.add('show-sidebar')
-  wrapper.classList.remove('margin')
 })
 
 
